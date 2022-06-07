728ff5a53e61e4c4f93ea2b0c6755d73072d3b1c
Abu Sayeed <abusayeed33343536@gmail.com>
2022-06-07 14:06:11 +0600

    addmoney2

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Addmoney;
+use App\Models\AddMoney;
 
 class BackendController extends Controller
 {
